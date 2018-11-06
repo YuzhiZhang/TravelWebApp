eebbd07bb40eba3cd9a50cbd48f0c66a41ab0859
zhangyuzhi <zhangyuzhi1993@gmail.com>
2018-11-06 11:00:10 +0800

    add eslint/prettier rules(添加代码质量检查与格式化验证规则)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 尽可能地使用单引号,允许字符串使用单引号或双引号，只要字符串中包含了一个其它引号
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-useless-escape': 0,
+    'no-control-regex': 0
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
-};
+}
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+// .prettierrc.js
+module.exports = {
+  printWidth: 100,
+  singleQuote: true, // 这个地方需要跟 eslint 的规则保持一致
+  semi: false // 这个地方需要跟 eslint 的规则保持一致
+}
