5bf868abd0e5686dc1d1c58f8b5309d2bbf06454
zhangyuzhi <zhangyuzhi1993@gmail.com>
2018-11-08 17:18:18 +0800

    add header(添加home中的header组件)

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'reset-css/reset.css'
+import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
---
+++ src/views/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeHeader from '../../components/home/Header.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
