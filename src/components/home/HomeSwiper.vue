3e69d6b60e9697632f96bbd9c3ef7bdbeb27c236
zhangyuzhi <zhangyuzhi1993@gmail.com>
2018-11-09 17:04:50 +0800

    change(添加home中的轮播组件)

---
+++ src/components/home/HomeSwiper.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <!--<div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+      <div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false
+        }
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1811/d4/7fa0b173c269f802.jpg_750x200_b8d88f51.jpg'
+        },
+        {
+          id: '002',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1809/62/04400e9b345de002.jpg_750x200_1af72d73.jpg'
+        },
+        {
+          id: '003',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1809/ae/7518c2fbc1415202.jpg_750x200_09bbe391.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.67%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
