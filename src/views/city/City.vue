33c93cb513e25e4d31c59fdd204e02d98b7c0d86
zhangyuzhi <zhangyuzhi1993@gmail.com>
2018-11-22 17:48:17 +0800

    add city-list branch

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -2,17 +2,20 @@
   <div class="city">
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 
 <script>
 import CityHeader from '@/components/city/CityHeader'
 import CitySearch from '@/components/city/CitySearch'
+import CityList from '@/components/city/CityList'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@
 module.exports = {
   devServer: {
     port: 8088,
+    disableHostCheck: true,
     proxy: {
       '/api': {
         target: 'http://192.168.137.1:8088',
