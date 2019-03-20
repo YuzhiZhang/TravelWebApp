8f6fa733945d496b9283eec2b0d3b571039269e9
zhangyuzhi <zhangyuzhi1993@gmail.com>
2019-03-20 15:22:11 +0800

    finish city alphabet

--- src/components/city/CityAlphabet.vue
+++ src/components/city/CityAlphabet.vue
@@ -19,10 +19,15 @@ export default {
 <style scoped lang="stylus">
   .list
     display flex
+    flex-direction column
+    justify-content center
     position absolute
     top 1.58rem
     right 0
     bottom 0
     width .4rem
-    background red
+    .item
+      line-height .4rem
+      text-align center
+      color $bgColor
 </style>
