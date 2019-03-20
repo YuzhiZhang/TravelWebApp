b4e640f3cc427f354dbbcabd9d33679a19be2efc
zhangyuzhi <zhangyuzhi1993@gmail.com>
2019-03-20 18:00:28 +0800

    add city-ajax

--- src/components/city/CityList.vue
+++ src/components/city/CityList.vue
@@ -12,75 +12,17 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">深圳</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">深圳</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">深圳</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">深圳</div>
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{ item.name }}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="title border-topbottom">{{ key }}</div>
         <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-      </div><div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
+            {{ innerItem.name }}
+          </div>
         </div>
       </div>
     </div>
@@ -91,6 +33,20 @@
 import BScroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: {
+      default: function() {
+        return []
+      },
+      type: Array
+    },
+    cities: {
+      default: function() {
+        return {}
+      },
+      type: Object
+    }
+  },
   data() {
     return {
       scroll: null
@@ -103,39 +59,39 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-  .border-topbottom
-    &:before
-      border-color #ccc
-    &:after
-      border-color #ccc
-  .border-bottom
-    &:before
-      background #ccc
-  .list
+.border-topbottom
+  &:before
+    border-color #ccc
+  &:after
+    border-color #ccc
+.border-bottom
+  &:before
+    background #ccc
+.list
+  overflow hidden
+  position absolute
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  .title
+    line-height .54rem
+    background #eee
+    padding-left .2rem
+    font-size .26rem
+  .button-list
     overflow hidden
-    position absolute
-    top 1.58rem
-    left 0
-    right 0
-    bottom 0
-    .title
-      line-height .54rem
-      background #eee
-      padding-left .2rem
-      font-size .26rem
-    .button-list
-      overflow hidden
-      padding .1rem .6rem .1rem .1rem
-      .button-wrapper
-        float left
-        width 33.33%
-        .button
-          margin .1rem
-          padding .1rem 0
-          text-align center
-          border .02rem solid #ccc
-          border-radius .06rem
-    .item-list
-      line-height .76rem
-      padding-left .2rem
+    padding .1rem .6rem .1rem .1rem
+    .button-wrapper
+      float left
+      width 33.33%
+      .button
+        margin .1rem
+        padding .1rem 0
+        text-align center
+        border .02rem solid #ccc
+        border-radius .06rem
+  .item-list
+    line-height .76rem
+    padding-left .2rem
 </style>
