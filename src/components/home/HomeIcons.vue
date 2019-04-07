55f06627fcd8c9bff2b67f6169b4c8ca5de4e46d
Yuzhi Zhang <zhangyuzhi1993@gmail.com>
2019-04-07 15:43:46 +0800

    format home-icon

--- src/components/home/HomeIcons.vue
+++ src/components/home/HomeIcons.vue
@@ -2,15 +2,11 @@
   <div class="icons">
     <swiper :options="swiperOption">
       <swiper-slide v-for="(page, index) in pages" :key="index">
-        <div
-          class="icon"
-          v-for="item in page"
-          :key="item.id"
-        >
+        <div class="icon" v-for="item in page" :key="item.id">
           <div class="icon-img">
-            <img :src="item.imgUrl" alt="" class="icon-img-content">
+            <img :src="item.imgUrl" alt="" class="icon-img-content" />
           </div>
-          <p class="icon-desc">{{item.desc}}</p>
+          <p class="icon-desc">{{ item.desc }}</p>
         </div>
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
@@ -50,42 +46,42 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-  @import "~@/assets/styles/varibles.styl"
-  @import "~@/assets/styles/mixins.styl"
-  .icons >>> .swiper-container
+@import "~@/assets/styles/varibles.styl"
+@import "~@/assets/styles/mixins.styl"
+.icons >>> .swiper-container
+  height 0
+  padding-bottom 55%
+  .swiper-pagination
+    bottom 3px
+.icons
+  margin-top .1rem
+  .icon
+    position relative
+    overflow hidden
+    float left
+    width 25%
     height 0
-    padding-bottom 55%
-    .swiper-pagination
-      bottom 3px
-  .icons
-    margin-top .1rem
-    .icon
-      position relative
-      overflow hidden
-      float left
-      width 25%
-      height 0
-      padding-bottom 25%
-      .icon-img
-        position absolute
-        left 0
-        top 0
-        right 0
-        bottom .44rem
-        box-sizing border-box
-        padding-bottom .1rem
-        .icon-img-content
-          display block
-          margin 0 auto
-          height 100%
-      .icon-desc
-        position absolute
-        left 0
-        right 0
-        bottom 0
-        height .44rem
-        line-height .44rem
-        text-align center
-        color $darkTextColor
-        ellipsis()
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      left 0
+      top 0
+      right 0
+      bottom .44rem
+      box-sizing border-box
+      padding-bottom .1rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .44rem
+      line-height .44rem
+      text-align center
+      color $darkTextColor
+      ellipsis()
 </style>
